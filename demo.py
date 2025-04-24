656d247b0156dd722d9849e7733bc6fe0fabff4e
Chetanghotekar <chetan.ghotekar_24uds@sanjivani.edu.in>
2025-04-24 10:51:02 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,9 @@
 https://github.com/akashkolhe0512/cg38/blob/main/demo.py
+
+Set 5:
+1. Navigate Jira to find the active sprint board and sprint backlog.
+2. Create an epic and link stories to it in Jira.
+3. Write a JQL to find all unresolved tasks assigned to Team-A.
+4. Merge changes into the main branch after Pull requests approval.
+5. JQL: Filter issues where assignee is EMPTY (unassigned).
+6. Merge changes into the main branch after Pull request approva
